c4f4c6b71b8fc27d7ba1d3b67920695218534380
Shakti K.C <107423716+shakti-kc@users.noreply.github.com>
2023-08-21 10:23:36 +0545

    Time: 22 ms (27.01%), Space: 13.3 MB (27.64%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, digits):
+        to_str_arr =(map(str, digits))
+        to_str = "".join(to_str_arr)
+
+        to_int = int(to_str)
+        add_one = to_int+1
+
+        new_arr =[]
+        to_strA = str(add_one)
+        for x in to_strA:
+            new_arr.append(x)
+
+
+        new_arr = list(map(int, new_arr))
+        return (new_arr)
